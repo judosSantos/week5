7a0c8a2e348bf830f65184d2b4b0d40608101208
judossantos13 <judossantos13@gmail.com>
2026-08-13 22:55:03 +0200

    add footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ function App() {
   return (
     <div className="App">
       <header>Hallo Welt</header>
+      <footer>
+        This project was coding by JuSantos and hosted on{" "}
+        <a
+          href="https://github.com/judosSantos/week5.git"
+          target="_blank"
+          rel="github link"
+        >
+          GitHub
+        </a>
+      </footer>
     </div>
   );
 }
