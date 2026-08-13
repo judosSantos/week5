15e77ad57bc311efbae2542cee7f6ae57b44ddbd
judossantos13 <judossantos13@gmail.com>
2026-08-13 23:10:05 +0200

    added bootsprap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <header>Hallo Welt</header>
-      <footer>
-        This project was coding by JuSantos and hosted on{" "}
-        <a
-          href="https://github.com/judosSantos/week5.git"
-          target="_blank"
-          rel="github link"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <header>Hallo Welt</header>
+
+        <footer>
+          This project was coding by JuSantos and hosted on{" "}
+          <a
+            href="https://github.com/judosSantos/week5.git"
+            target="_blank"
+            rel="github link"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
